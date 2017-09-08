e56aeecd7978529b254bb4d4e2e9027d343851d0
Sebastián D'Agostino <sebastiandagostino@users.noreply.github.com>
2017-09-08 11:15:43 -0300

    Format code in Link.cpp

--- src/Link.cpp
+++ src/Link.cpp
@@ -1,33 +1,33 @@
 #include "Link.h"
 
 Link::Link(int toNodeId, int totalLatency) :
-	toNodeId(toNodeId), totalLatency(totalLatency), sendTime(0), messages(0) {
+    toNodeId(toNodeId), totalLatency(totalLatency), sendTime(0), messages(0) {
 }
 
 int Link::getToNodeId() const {
-	return toNodeId;
+    return toNodeId;
 }
 
 int Link::getReceiveTime() const {
-	return sendTime + totalLatency;
+    return sendTime + totalLatency;
 }
 
 int Link::getSendTime() const {
-	return sendTime;
+    return sendTime;
 }
 
 void Link::setSendTime(int sendTime) {
-	this->sendTime = sendTime;
+    this->sendTime = sendTime;
 }
 
 int Link::getTotalLatency() const {
-	return totalLatency;
+    return totalLatency;
 }
 
 Message* Link::getMessages() {
-	return messages;
+    return messages;
 }
 
 void Link::setMessages(Message* messages) {
-	this->messages = messages;
+    this->messages = messages;
 }
